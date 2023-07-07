1810b0dda41604abb50cf1ce09e9556a797141a7
ris4rob <108408741+ris4rob@users.noreply.github.com>
2023-07-07 17:35:49 +0800

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function (array, ...args) {
-  return array.filter((arr) => arr != args);
+  // argsArray turns all argument into an array using Array.from(arguments)
+  let argsArray = Array.from(arguments);
+
+  // remove original array
+  argsArray.shift();
+
+  // filter each argument value from array
+  const filterArray = array.filter((arg) => argsArray.indexOf(arg) === -1);
+
+  // return filtered array
+  return filterArray;
+
+  // return array.filter((arr) => arr != args);
 };
 
 // Do not edit below this line
