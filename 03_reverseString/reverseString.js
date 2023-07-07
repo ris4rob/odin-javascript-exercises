a169245f13a85f7ca8b26d1f618cae91f35cb372
ris4rob <108408741+ris4rob@users.noreply.github.com>
2023-07-07 10:53:29 +0800

    Refactored function to one line using string methods instead of loops

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  // let result = '';
+  // let length = string.length;
 
+  // if (string === '') {
+  //   return string;
+  // } else {
+  //   for (let i = 1; i < length; i++) {
+  //     result += string[length - i];
+  //   }
+  //   result = result.concat(string[0]);
+  //   return result;
+  // }
+  // refactored code here to one line
+  return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
