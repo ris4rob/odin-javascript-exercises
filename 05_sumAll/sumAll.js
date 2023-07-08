7dbb32e9efc163adc2a883fc7a22aea6477043aa
ris4rob <108408741+ris4rob@users.noreply.github.com>
2023-07-08 11:03:54 +0800

    Complete first 3 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let result = 0;
+  do {
+    result += a;
+    a++;
+  } while (a <= b);
+  return result;
 };
 
 // Do not edit below this line
