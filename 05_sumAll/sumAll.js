510702e874bc34af60776ccfee6a8610d2e2fc9e
ris4rob <108408741+ris4rob@users.noreply.github.com>
2023-07-10 10:25:13 +0800

    Nested loop inside typeof loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,21 @@
 const sumAll = function (a, b) {
   let result = 0;
-  do {
-    result += a;
-    a++;
-  } while (a <= b);
-  return result;
-};
+  
+  if typeof a === 'number' && typeof b === 'number' {
+    if a < b {
+      do {
+        result += a;
+        a++;
+      } while (a <= b);
+      return result;
+    } else if b > a {
+      do {
+        result += b;
+        b++;
+      } while (b <= a);
+      return result
+    }
+  }
 
 // Do not edit below this line
 module.exports = sumAll;
