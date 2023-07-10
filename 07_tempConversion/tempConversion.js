9161d2deef1e16a4a382e315b30162e1c433b6c1
ris4rob <108408741+ris4rob@users.noreply.github.com>
2023-07-10 15:14:47 +0800

    Complete tempConversion functions all tests PASS

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  let fahrenheit = (temperature - 32) / 1.8;
+
+  if (fahrenheit % 1 === 0) {
+    return fahrenheit;
+  } else {
+    return parseFloat(fahrenheit.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  let celsius = temperature * 1.8 + 32;
+
+  if (celsius % 1 === 0) {
+    return celsius;
+  } else {
+    return parseFloat(celsius.toFixed(1));
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
